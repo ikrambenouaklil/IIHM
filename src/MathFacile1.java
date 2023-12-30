9610b63ec741264284c3fde43695b523abc28c44
ikram <ikrambn2002@gmail.com>
2023-12-30 13:55:48 +0100

    o

--- src/MathFacile1.java
+++ src/MathFacile1.java
@@ -21,7 +21,7 @@ public class MathFacile1 extends JFrame {
       
         setTitle("Baby Bear");
         setSize(500, 500);
-    //    setLayout(new BorderLayout());
+        setLayout(new BorderLayout());
         /* add background  */
         
         JLabel background=new JLabel(new ImageIcon("qst bg.png"));
@@ -30,14 +30,17 @@ public class MathFacile1 extends JFrame {
         
         /*------------------   add panels  -----------------*/
    
-        JPanel panel = new JPanel();
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
-   
+        JPanel panel = new JPanel();// cote 
+        
+        JPanel panel1 = new JPanel();//li fl wst 
+       
+        JPanel panel2 = new JPanel();// south 
+ 
         panel.setBackground(Color.red);
         panel1.setBackground(Color.blue);
         panel2.setBackground(Color.yellow);
-                
+//panel 1 n9smo 
+        panel1.setLayout(new BorderLayout());
 
         // panel.setPreferredSize(new Dimension(100,100));//EAST
         // panel1.setPreferredSize(new Dimension(100,100));//CENTER
@@ -50,16 +53,38 @@ public class MathFacile1 extends JFrame {
         ImageIcon imageMATHQSTF = new ImageIcon("math1+2.png"); 
         JLabel qstbar = new JLabel();
         qstbar.setIcon(imageMATHQSTF);
-        
+        qstbar.setHorizontalAlignment(JLabel.CENTER);
 
 
-        // les choix 
-
+        // les choix
+        JPanel southChoix = new JPanel();  
+        southChoix.setLayout(new BorderLayout());
+        //------------row 1 ------------
+        JPanel Vertical1 = new JPanel() ; 
         JButton choix1 = CreateButt.createTransparentButton("3", "labelchoix.png");
         JButton choix2 = CreateButt.createTransparentButton("4", "labelchoix.png");
+    //labels => vertical 1 
+        Vertical1.setLayout(new BorderLayout()); 
+        Vertical1.add(choix1,BorderLayout.NORTH);
+        Vertical1.add(choix2,BorderLayout.SOUTH);
+
+
+
+southChoix.add(Vertical1,BorderLayout.EAST); 
+
+
+//row 2 
+JPanel Vertical2 = new JPanel() ; 
         JButton choix3 = CreateButt.createTransparentButton("6", "labelchoix.png");
         JButton choix4 = CreateButt.createTransparentButton("10", "labelchoix.png");
-     
+     //labels => vertical 2
+     Vertical2.setLayout(new BorderLayout()); 
+     Vertical2.add(choix3,BorderLayout.NORTH);
+     Vertical2.add(choix4,BorderLayout.SOUTH);
+
+     southChoix.add(Vertical2,BorderLayout.WEST); 
+
+
         /*---------------menu button-------  */
         JButton menu =   CreateButt.ButtonIcons( ""); 
         panel.add(menu); 
@@ -68,16 +93,14 @@ public class MathFacile1 extends JFrame {
 
 
 
-       panel1.add(qstbar);
-       panel1.add(choix1); 
-       panel1.add(choix2);
-       panel1.add(choix3);
-       panel1.add(choix4);
-       
+       panel1.add(qstbar,BorderLayout.NORTH);
+       panel1.add(southChoix,BorderLayout.SOUTH ); 
+
        setLocationRelativeTo(null);// centrer le frame
        background.add(panel,BorderLayout.EAST);
        background.add(panel1 ,BorderLayout.CENTER);
        background.add(panel2,BorderLayout.SOUTH);
+       
 
        /*make the panels bg transparent  */
     //  panel.setOpaque(false);
